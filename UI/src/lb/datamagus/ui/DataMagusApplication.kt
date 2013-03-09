e4e5fa7099260da0a54e9361b40dbee64b3c8e46
Leonid Bushuev <leonidos@gmail.com>
2013-03-09 01:19:42 +0100

    Menu that does nothing.

--- UI/src/lb/datamagus/ui/DataMagusApplication.kt
+++ UI/src/lb/datamagus/ui/DataMagusApplication.kt
@@ -1,13 +1,10 @@
 package lb.datamagus.ui
 
 import javafx.application.Application
+import javafx.scene.*
+import javafx.scene.control.*
+import javafx.scene.layout.*
 import javafx.stage.Stage
-import sun.launcher.resources.launcher
-import javafx.scene.layout.BorderPane
-import javafx.scene.Scene
-import javafx.scene.layout.StackPane
-import javafx.scene.Parent
-
 
 class DataMagusApplication: Application()
 {
@@ -16,10 +13,33 @@ class DataMagusApplication: Application()
     {
         primaryStage.setTitle("DataMagus")
 
-        val rootPane = BorderPane()
+
+        val rootPane = VBox()
         val scene = Scene(rootPane:Parent, 600.0, 600.0)
+
+        val menuBar = makeMenu()
+        rootPane.getChildren().addAll(menuBar)
+
         primaryStage.setScene(scene)
         primaryStage.show()
+    }
+
+
+    fun makeMenu(): MenuBar
+    {
+        val menuBar = MenuBar()
+
+        val menuProject = Menu("Project")
+        menuProject.getItems().addAll(MenuItem("New…"), SeparatorMenuItem(), MenuItem("Quit"))
+
+        val menuEdit = Menu("Edit")
+        menuEdit.getItems().addAll(MenuItem("Copy"), MenuItem("Paste"))
+
+        val menuDatabase = Menu("Database")
+        menuBar.getMenus().addAll(menuProject, menuEdit, menuDatabase)
+        menuBar.setUseSystemMenuBar(true)
+
+        return menuBar
     }
 
 }
